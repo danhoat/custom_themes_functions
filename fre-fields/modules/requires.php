cf573209862bdf5fcf051e7923c651b0df0898ab
danng <danhoat@gmail.com>
2024-07-01 10:49:04 +0700

    update thêm

--- fre-fields/modules/requires.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-function box_tooltips($msg){?>
-<span  class=" tooltip-hover" data-bs-toggle="tooltip" data-bs-html="true" title="<?php echo $msg;?>"> <i class="fa fa-info-circle"></i></span>
-
-<?php }
-require(dirname(__FILE__).'/ae_user_fields.php');
